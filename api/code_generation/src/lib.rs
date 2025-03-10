45d5bf9696f4b76c668bb6a22983ae3ca036d516
waku <wakunguma13@gmail.com>
2025-03-10 11:34:16 -0600

    moved the parsing to the proc macro

--- api/code_generation/src/lib.rs
+++ api/code_generation/src/lib.rs
@@ -1,11 +1,20 @@
-use proc_macro::TokenStream;
 use quote::quote;
+use std::fs;
+use core_types::ApiSchema;
 
+/// Generate code from a schema file
 #[proc_macro]
-pub fn generate_code(input:proc_macro::TokenStream) -> proc_macro::TokenStream{
-	let expanded = quote! {
-		println!("Hello world")
-	};
+pub fn generate_code(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let path = input.to_string().replace("\"", "");
+	
+	let contents = fs::read_to_string(path).unwrap();
+    let data: ApiSchema = serde_yaml::from_str(&contents).unwrap();
 
-	proc_macro::TokenStream::from(expanded)
-}	
+    println!("{:#?}", data);
+
+    let expanded = quote! {
+        println!("Hello world")
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
--- api/compiler/src/main.rs
+++ api/compiler/src/main.rs
@@ -1,13 +1,7 @@
-use std::fs;
 use code_generation::generate_code;
 use core_types::ApiSchema;
-
+use std::fs;
 
 fn main() {
-	let contents = fs::read_to_string("../schema.yml").unwrap();
-	let data: ApiSchema = serde_yaml::from_str(&contents).unwrap();
-	
-	generate_code!(data);
-	
-	println!("{:#?}",data);
+    generate_code!("../schema.yml");
 }
