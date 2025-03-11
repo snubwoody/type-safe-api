72faa8e4f738b175126eb50c225624ceee567d7e
waku <wakunguma13@gmail.com>
2025-03-11 10:51:03 -0600

    the typescipt type code gen is now working

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+interface Error{
+	description: string,
+	details: string,
+	code: number,
+}
+
+interface UserPayload{
+	id: string,
+}
+
+interface User{
+	created_at: string,
+	name: string,
+	id: number,
+	email: string,
+}
+
